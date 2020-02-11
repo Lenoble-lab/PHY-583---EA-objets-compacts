85601ea1c0ea009a0998d963b9abc4e8126c12b9
Thomas BELLIER <thomas.bellier@polytechnique.edu>
2020-02-11 17:51:01 +0100

    update Thomas

--- Thomas/lightcones.py
+++ Thomas/lightcones.py
@@ -15,11 +15,28 @@ class LightCone:
         self.r=r
         self.metric = metric
         (self.times,self.radius) = metric.cones(r,step,n,normalized)
+    def plot(self,figure, shift=0, label=None):
+        plt.figure(figure)
+        plt.plot(self.radius+shift,self.times,label=label)
+
+class Freefall:
+    def __init__(self,r0,metric,steps,time,normalized=True):
+        self.r0 = r0
+        self.metric = metric
+        (self.times,self.radius) = metric.fall(r0,steps,time,normalized)
+        (self.proper_times,self.proper_radius) = metric.fall_propre(r0,steps,time,normalized)
+    def plot(self,figure, shift=0, label=None):
+        plt.figure(figure)
+        plt.plot(self.radius,self.times+shift,label=label)
+    def plot_proper(self,figure, shift=0, label=None):
+        plt.figure(figure)
+        plt.plot(self.proper_radius,self.proper_times+shift,label=label)
 
 class Metric:
     def __init__(self,M):
         self.M = M
         self.Rs = 2*cst.G*M/(cst.c**2)
+    
 
 class Schwarzschild(Metric):
     def __init__(self,M):
@@ -51,6 +68,48 @@ class Schwarzschild(Metric):
         return (1-1/r)
     def cone_n(self,t,r):
         return -(1-1/r)
+    def fall(self,r0,steps,time,normalized):
+        step = time / steps
+        if not normalized:
+            step/=self.Rs
+            r0/=self.Rs
+        self.r0 = r0
+        radius = [r0]
+        times = [0]
+        integrator = ode(self.fall_eq)
+        integrator.set_integrator("vode")
+        integrator.set_initial_value(r0-r0*np.finfo(float).epsneg,0)
+        for k in range(steps):            
+            radius.append(integrator.integrate(integrator.t+step))
+            times.append(integrator.t)
+        (times,radius)=(np.array(times),np.array(radius))
+        if not normalized:
+            times = times*self.Rs
+            radius = radius*self.Rs
+        return (times,radius)
+    def fall_eq(self,t,r):
+        return -np.sqrt((1/r-1/self.r0)*(1-1/r)/(1-1/self.r0))
+    def fall_propre(self,r0,steps,time,normalized):
+        step = time / steps
+        if not normalized:
+            step/=self.Rs
+            r0/=self.Rs
+        self.r0 = r0
+        radius = [r0]
+        times = [0]
+        integrator = ode(self.fall_eq_propre)
+        integrator.set_initial_value(r0-r0*np.finfo(float).epsneg,0)
+        for k in range(steps):            
+            radius.append(integrator.integrate(integrator.t+step))
+            times.append(integrator.t)
+        (times,radius)=(np.array(times),np.array(radius))
+        if not normalized:
+            times = times*self.Rs
+            radius = radius*self.Rs
+        return (times,radius)
+    def fall_eq_propre(self,t,r):
+        return -np.sqrt((1/r-1/self.r0))
+        
         
 class EddingtonFinkelstein(Metric):
     def __init__(self,M):
@@ -80,3 +139,18 @@ class EddingtonFinkelstein(Metric):
         return ((r-1)/(r+1))
     def cone_n(self,t,r):
         return -1
+
+class Lemaitre(Metric):
+    def __init__(self,M):
+        super().__init__(M)
+    def cones(self,cT,step,n,normalized):
+        if not normalized:
+            step/=self.Rs
+            r/=self.Rs
+        return
+
+
+
+
+
+
--- Thomas/test.py
+++ Thomas/test.py
@@ -8,6 +8,8 @@ Created on Tue Feb  4 14:07:01 2020
 import matplotlib.pyplot as plt
 from lightcones import *
 import scipy.constants as cst
+import scipy.integrate as sc
+
 
 #M = 10**30
 #Rs = 2*cst.G*M/(cst.c**2)
@@ -17,22 +19,62 @@ import scipy.constants as cst
 
 M = 1
 r = [0.5,0.8,0.95,1.05,1.2,1.5,2,3,5]
-n = 5000
-step = 10**-3
+n = 50000
+step = 10**-4
+time = 25
+steps = 100000
+cones = 7
+
+metric = Schwarzschild(M)
+test_fall = Freefall(5,metric,steps,time)
+
 
 plt.figure(1)
-metric = Schwarzschild(M)
-test_cones = [LightCone(k,metric,step,n) for k in r]
+plt.plot(test_fall.times,test_fall.radius,label="observateur à l'infini")
+#plt.plot(test_fall.proper_times,test_fall.proper_radius,label="temps propre")
 
-for cones in test_cones:
-    plt.plot(cones.radius,cones.times)
+#light_cones = [LightCone(test_fall.radius[k*(steps-1)//(cones-1)],metric,step,n) for k in range(cones)]
+#
+#
+#for k in range(cones):
+#    cone = light_cones[k]
+#    shift = test_fall.times[k*(steps-1)//(cones-1)]
+#    plt.plot(cone.times+shift,cone.radius)
+
+plt.ylim(bottom=0)
+plt.axhline(y=1, color="black")
+plt.title("Chute libre dans une métrique de Schwarzschild")
+plt.ylabel("r/Rs")
+plt.xlabel("ct/Rs")
+plt.legend()
 
 
-plt.figure(2)
-metric = EddingtonFinkelstein(M)
-test_cones = [LightCone(k,metric,step,n) for k in r]
+#test_cones = [LightCone(k,metric,step,n) for k in r]
 
-for cones in test_cones:
-    plt.plot(cones.radius,cones.times)
+#for cones in test_cones:
+#    cones.plot(1,label="r/Rs = " + str(cones.r))
+
+
+#plt.figure(1)
+#plt.xlim(left=0)
+#plt.axvline(x=1, color="black")
+#plt.title("Chute libre dans une métrique de Schwarzschild")
+#plt.xlabel("r/Rs")
+#plt.ylabel("ct/Rs")
+#plt.legend()
+
+#metric = EddingtonFinkelstein(M)
+#test_cones = [LightCone(k,metric,step,n) for k in r]
+#
+#for cones in test_cones:
+#    cones.plot(2,label="r/Rs = " + str(cones.r))
+#
+#plt.figure(2)
+#plt.xlim(left=0)
+#plt.axvline(x=1, color="black")
+#plt.title("Cones de lumière dans une métrique de Eddington-Finkelstein")
+#plt.xlabel("r/Rs")
+#plt.ylabel("ct")
+#plt.legend()
 
 plt.show()
